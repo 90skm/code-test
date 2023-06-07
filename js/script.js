9ca76183ddf3f4e5cae1be2e8282f4f711176598
gchaithra <mittalsachin.2012@gmail.com>
2023-06-07 15:05:27 +0530

    Latest New Code

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-fetch('json/code-test.json')
+let get_sales_data = async() => {
+
+await fetch('json/code-test.json')
       .then(function (response) {
                return response.json();
             })
@@ -6,10 +8,13 @@ fetch('json/code-test.json')
 			   var mainContainer = document.getElementById("jsondata");
             for (var i = 0; i < data.articles.length; i++) {
 			var div = document.createElement("div");
-                 div.innerHTML = '<div class="left-content"><h5 class="category"><a href="'+data.articles[i].link+'" target="_blank" title="'+data.articles[i].primarySectionRouteName+'">'+data.articles[i].primarySectionRouteName+'</h5></a><h2 class="heading-class">'+data.articles[i].headline+'</h2><p class="description">'+data.articles[i].standfirst+'.</p></div><div class="right-content"><div class="fake-img" ><img src="'+data.articles[i].thumbnail.src+'" alt="'+data.articles[i].thumbnail.title+'"></div></div><div class="border-line"></div>';
+                 div.innerHTML = '<div class="container"><div class="left-content"><h5 class="category"><a href="'+data.articles[i].link+'" target="_blank" title="'+data.articles[i].primarySectionRouteName+'">'+data.articles[i].primarySectionRouteName+'</h5></a><h2 class="heading-class">'+data.articles[i].headline+'</h2><p class="description">'+data.articles[i].standfirst+'.</p></div><div class="right-content"><div class="fake-img" ><img src="'+data.articles[i].thumbnail.src+'" alt="'+data.articles[i].thumbnail.title+'"></div></div></div><hr>';
                 mainContainer.appendChild(div);
             }
             })
             .catch(function (err) {
                 return err;
             });
+			
+}
+get_sales_data();
